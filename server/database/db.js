eb30f59e3a85fe08ff269039faa47ceed27af077
dipansh17 <dipanshmalhotra11@gmail.com>
2023-08-23 10:22:10 +0530

    Added dotenv and url

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,16 @@
+// require(`dotenv`).config();
+import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose from "mongoose";
 const Connection = async (
   username = "dipanshmalhotra11",
   password = "atqdWOO2vSBRInYH"
 ) => {
   // const URL = `mongodb://localhost:27017/google-doc`;
-  const URL = `mongodb+srv://dipanshmalhotra11:atqdWOO2vSBRInYH@google-docs-clone.syeouui.mongodb.net/?retryWrites=true&w=majority`;
+  // const URL = `mongodb+srv://dipanshmalhotra11:atqdWOO2vSBRInYH@google-docs-clone.syeouui.mongodb.net/?retryWrites=true&w=majority`;
   // const URL = `mongodb+srv://felixfelix:Felix%40453@cluster0.4oc4hbc.mongodb.net/sheetsDB`;
+  const URL = process.env.URL;
   try {
     await mongoose.connect(URL, {
       useUnifiedTopology: true,
